f9a6d4af1508a9529d8fe0cbec6bc9d23ba3e982
soumyareddy15 <mattesoumyareddy@gmail.com>
2021-07-27 15:33:38 +0530

    dotnet

---
+++ prjfirstapplication/Array_Static.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjfirstapplication
+{
+    //by default access specifier for class is internal
+    class ArrayEg
+    {   //by default access specifier for class is internal
+
+        //single dimensioanl array
+        //datatype[] arrayname=new datatype[size];
+
+        string[] fruits = new string[3];
+        int[] mark = { 89, 78, 90 };
+
+     internal   void GetFruits()
+        {
+            for (int i = 0; i < fruits.Length;i++)
+            {
+                Console.WriteLine("enter the fruitname");
+                
+                fruits[i] = Console.ReadLine();
+            }
+            foreach(var fu in fruits)
+            {
+                Console.WriteLine("Fruitanme:{0}", fu);
+            }
+        }
+    }
+    class StringEg
+    {
+        internal void StringFunctionEg()
+        {
+            string Firstname = "Sai";
+            string Name = "Monisha Devi";
+            Console.WriteLine("TO UPPER:{0}",Firstname.ToUpper());
+            Console.WriteLine("TO LOWER:{0}", Firstname.ToLower());
+            Console.WriteLine("Length{0}",Name.Length);
+            bool isContains = Name.Contains("UP");
+            Console.WriteLine("Contains up:{0}",isContains);
+            Console.WriteLine("substring : {0}",Name.Substring(3,5));
+        }
+    }
+    class Array_Static
+    {
+        static void Main()
+        {
+            ArrayEg arrayEg = new ArrayEg();
+            arrayEg.GetFruits();
+
+            StringEg stringEg = new StringEg();
+            stringEg.StringFunctionEg();
+        }
+    }
+}
---
+++ prjfirstapplication/Properties/Saledetails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day1assignment
+{
+    class Saledetails
+    {
+        int Salesno;
+        int Productno;
+        double Price;
+        DateTime dateofsale;
+        int Qty;
+        
+        Saledetails(int Salesno, int Productno, double Price, DateTime dateofsale, int Qty)
+        {
+            this.Salesno = Salesno;
+            this.Productno = Productno;
+            this.Price = Price;
+            this.dateofsale = dateofsale;
+            this.Qty = Qty;
+        }
+        void Sales(int qty, double price)
+        {
+            double TotalAmount = qty * price;
+            Console.WriteLine("Total Amount : {0}", TotalAmount);
+        }
+
+        void show_data()
+        {
+            Console.WriteLine("Sale no:{0} || Product no:{1} || Price:{2}  || Date of Sale :{3} || Quantity :{4}",Salesno,Productno,Price,(dateofsale).ToString("dd/MM/yyyy"),Qty);
+        }
+
+        public static void Main(String[] args)
+        {
+            Saledetails s1 = new Saledetails(1, 101, 10000, Convert.ToDateTime("25/12/1999"), 200);
+
+            s1.show_data();
+            s1.Sales(2, 60.5);
+        }
+    }
+}
